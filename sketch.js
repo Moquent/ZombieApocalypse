e008524e87e828b81f0a3c8da829a53b197906c0
TejeshCodes <63721912+TejeshCodes@users.noreply.github.com>
2020-07-16 11:33:46 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,12 +1,12 @@
 var roomimg, garimg, plimg, waterimg, foodp1, foodp2, foodp3, zomp1, zomp2, toolp1, toolp2, toolp3, rockp1, runbg;
 
-var player, zombie = [],
-  obs, button, g = 0;
-var gamestate = "welcome";
-var water1, water2, water3, food1, food2, food3, tool1, tool2, tool3;
+var player, zombie = [], p = 0,
+  obs, button, invisg;
+var gamestate = "run";
+var water1, water2, water3, food1, food2;
 var ground, i = 0;
-var rtime = 7,
-  gtime = 7,
+var rtime = 5,
+  gtime = 5,
   watercount = 0,
   foodcount = 0,
   totfood = 0,
@@ -20,14 +20,10 @@ function preload() {
   foodp1 = loadImage("sprites/food1.png");
   foodp2 = loadImage("sprites/food2.png");
   foodp3 = loadImage("sprites/food3.png");
-  zomp1 = loadImage("sprites/knife.png");
+  zomp1 = loadImage("sprites/zombie.gif");
   rockp1 = loadImage("sprites/rock1.png");
   zomp2 = loadImage("sprites/zombie.gif");
   runbg = loadImage("sprites/runbg.png");
-  toolp1 = loadImage("sprites/tool1.png");
-  toolp2 = loadImage("sprites/tool2.png");
-  toolp3 = loadImage("sprites/tool3.png");
-
 }
 
 function setup() {
@@ -36,6 +32,9 @@ function setup() {
   ground = createSprite(360, 200, 720, 400);
   ground.addImage(runbg);
   ground.scale = 1;
+
+  invisg = createSprite(360, 390, 1000, 20);
+  invisg.visible = false;
 
   food1 = createSprite(348, 290, 5, 5);
   food1.addImage(foodp1);
@@ -62,21 +61,11 @@ function setup() {
   water3.scale = 0.05;
   water3.rotation = 94;
 
-  // tool1 = createSprite(40, 300, 20, 20);
-  // tool1.addImage(toolp1);
-  // tool1.scale = 0.3;
-  
-  // tool2 = createSprite(40, 300, 20, 20);
-  // tool2.addImage(toolp2);
-  // tool2.scale = 0.3;
-  
-  // tool3 = createSprite(40, 300, 20, 20);
-  // tool3.addImage(toolp3);
-  // tool3.scale = 0.3;
-
   player = createSprite(40, 300, 20, 20);
   player.addImage(plimg);
   player.scale = 1;
+
+  obsgrp = createGroup();
 
   frameRate(30);
 
@@ -89,16 +78,15 @@ function draw() {
   console.log(mouseX, mouseY);
 
   if (gamestate == "welcome") {
-    player.visible = true;
+    background("red");
     water1.visible = false;
     water2.visible = false;
-    water3.visible = false;
+    water3.visible = false
     food1.visible = false;
     food2.visible = false;
     food3.visible = false;
     ground.visible = false;
-    // food2.visible = false;
-    // food2.visible = false;
+    player.visible = true;
 
     strokeWeight(2);
     stroke("BLACK");
@@ -122,21 +110,39 @@ function draw() {
   
   else if (gamestate == "room") {
     background(roomimg);
-    if(g == 0){
+    if(p == 0){  
       water1.visible = true;
       water2.visible = true;
-      water3.visible = true;
+      water3.visible = true
       food1.visible = true;
       food2.visible = true;
       food3.visible = true;
-      ground.visible = false;
-      g = 1;
+      p = 1;
     }
-      button.hide()
 
     if (frameCount % 30 == 0) {
       rtime -= 1;
     }
+    
+    if(mousePressedOver(water1)){
+      water1.visible = false;
+    }
+    if(mousePressedOver(water2)){
+      water2.visible = false;
+    }
+    if(mousePressedOver(water3)){
+      water3.visible = false;
+    }
+    if(mousePressedOver(food1)){
+      food1.visible = false;
+    }
+    if(mousePressedOver(food2)){
+      food2.visible = false;
+    }
+    if(mousePressedOver(food3)){
+      food3.visible = false;
+    }
+    
     fill("RED");
     textSize(25);
     text(rtime, 475, 30);
@@ -144,6 +150,50 @@ function draw() {
     if (rtime <= 0) {
       gamestate = "garage";
     }
+    strokeWeight(3);
+    stroke("BLACK");
+    fill("RED");
+    textSize(18);
+    text("FIND ALL THE FOOD AND WATER", 180, 30);
+  } 
+  
+  else if (gamestate == "garage") {
+    background(garimg);
+    if(p == 1){  
+      water1.visible = true;
+      water2.visible = true;
+      water3.visible = true
+      food1.visible = true;
+      food2.visible = true;
+      food3.visible = true;
+      p = 2;
+    }
+
+    if (frameCount % 30 == 0) {
+      gtime -= 1;
+    }
+    fill("RED");
+    textSize(25);
+    text(gtime, 475, 30);
+
+    if (gtime <= 0) {
+      gamestate = "run";
+    }
+
+    food1.x = 600;
+    food1.y = 220;
+    food2.x = 130;
+    food2.y = 235;
+    food3.x = 360;
+    food3.y = 230;
+    water1.x = 175;
+    water1.y = 118;
+    water1.scale = 0.05;
+    water2.x = 540;
+    water2.y = 250;
+    water2.scale = 0.05;
+    water3.x = 465;
+    water3.y = 145;
 
     if(mousePressedOver(water1)){
       water1.visible = false;
@@ -170,83 +220,23 @@ function draw() {
     textSize(18);
     text("FIND ALL THE FOOD AND WATER", 180, 30);
   } 
-
-  else if (gamestate == "garage") {
-    background(garimg);
-    if(g == 1){
-      water1.visible = true;
-      water2.visible = true;
-      water3.visible = true;
-      food1.visible = true;
-      food2.visible = true;
-      food3.visible = true;
-      ground.visible = false;
-      g = 0;
-    }
-      button.hide();
-
-    food1.x = 600;
-    food1.y = 220;
-    food2.x = 130;
-    food2.y = 235;
-    food3.x = 360;
-    food3.y = 230;
-    water1.x = 175;
-    water1.y = 118;
-    water1.scale = 0.05;
-    water2.x = 540;
-    water2.y = 250;
-    water2.scale = 0.05;
-    water3.x = 465;
-    water3.y = 145;
-
-    if (frameCount % 30 == 0) {
-      gtime -= 1;
-    }
-    fill("RED");
-    textSize(25);
-    text(gtime, 475, 30);
-
-    if (gtime <= 0) {
-      gamestate = "run";
-    }
-
-    if(mousePressedOver(water1)){
-      water1.visible = false;
-    }
-    if(mousePressedOver(water2)){
-      water2.visible = false;
-    }
-    if(mousePressedOver(water3)){
-      water3.visible = false;
-    }
-    if(mousePressedOver(food1)){
-      food1.visible = false;
-    }
-    if(mousePressedOver(food2)){
-      food2.visible = false;
-    }
-    if(mousePressedOver(food3)){
-      food3.visible = false;
-    }
-    
-    strokeWeight(3);
-    stroke("BLACK");
-    fill("RED");
-    textSize(18);
-    text("FIND ALL THE FOOD AND WATER", 180, 30);
-  }
   
   else if (gamestate == "run") {
-    background(runbg);
-    ground.visible = true;
-    water1.visible = false;
-    water2.visible = false;
-    water3.visible = false;
-    food1.visible = false;
-    food2.visible = false;
-    food3.visible = false;
-    button.hide()
+    background("yellow");
+    if(p == 0){
+      water1.visible = false;
+      water2.visible = false;
+      water3.visible = false
+      food1.visible = false;
+      food2.visible = false;
+      food3.visible = false;
+      ground.visible = true;
+      player.scale = 0.5;
+      player.x = 275
+      player.y = 330
+      button.hide()
+      p = 3;
+    }
 
     if (frameCount % 150 == 0) {
       zombie[i] = createSprite(50, 330, 10, 10);
@@ -254,20 +244,43 @@ function draw() {
       zombie[i].scale = 0.8;
     }
 
-    if (frameCount % 100 == 0) {
-      obs = createSprite(720, 360, 10, 10);
-      obs.velocityX = -4;
-      obs.addImage(rockp1);
-      obs.scale = 0.2
+    if(keyDown("space") && player.y >= 300){
+      player.velocityY = -12;
     }
-    player.scale = 0.5;
-    player.x = 275
-    player.y = 330
+
+    player.velocityY += 1;
+    if (frameCount % 100 == 0) {
+      obs = createSprite(720, 360, 50, 50);
+      obs.velocityX = -4;
+      // obs.addImage(rockp1);
+      // obs.scale = 0.2
+      obsgrp.add(obs)
+    }
     
+    player.collide(invisg);
+
+    if(obsgrp.isTouching(player)){
+      gamestate = "over";
+    }
     ground.velocityX = -4
-    if(ground.x < 20){
+    if(ground.x < 0){
       ground.x = 360;
     }
+  }
+  else if (gamestate = "over"){
+    background("black");
+    water1.visible = false;
+    water2.visible = false;
+    water3.visible = false
+    food1.visible = false;
+    food2.visible = false;
+    food3.visible = false;
+    button.hide()
+    ground.visible = false;
+    textSize(30);
+    fill("RED");
+    text("GAME OVER", 260, 150)
+    text("THANK YOU FOR PLAYING", 170, 220)
   }
 
   drawSprites();
